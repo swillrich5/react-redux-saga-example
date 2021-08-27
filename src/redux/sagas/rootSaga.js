753dcb559c29b90c0bad78a7449c12eda3d6b814
swillrich5 <swillrich@gmail.com>
2021-08-27 17:12:27 -0500

    initial commit after set up

---
+++ src/redux/sagas/rootSaga.js
@@ -0,0 +1,8 @@
+import { takeLatest } from 'redux-saga/effects';
+import { handleGetUser } from './handlers/user';
+import { GET_USER } from "../ducks/user";
+
+
+export function* watcherSaga() {
+    yield takeLatest(GET_USER, handleGetUser);
+};
